b9365a14b43c5534f6608d834bdc81e0679ab26f
Bastian Köcher <mail@kchr.de>
2017-05-28 22:49:22 +0200

    Adds configuration for my home tower

---
+++ home-tower.nix
@@ -0,0 +1,22 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the base configuration that is equal accross all my machines.
+      ./base-configuration.nix
+    ];
+
+  networking.hostName = "BastiTower-Nixos"; # Define your hostname.
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+
+  boot.loader.grub.useOSProber = true;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.driSupport32Bit = true;
+}
