818920dc4b0f6c5eda7c2a19f28cd9efc6436f7c
Bastian Köcher <git@kchr.de>
2018-07-03 21:04:52 +0200

    More babyphone config tweaks

--- babyphone.nix
+++ babyphone.nix
@@ -9,15 +9,18 @@
   networking.hostName = "babyphone";
 
   boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.loader.raspberryPi.enable = true;
+  boot.loader.raspberryPi.version = 3;
+  boot.loader.raspberryPi.uboot.enable = true;
+  boot.loader.raspberryPi.firmwareConfig = ''
+    start_x=1
+    gpu_mem=256
+  '';
+  services.nixosManual.enable = false;
 
   boot.kernelParams = [ "cma=32M" ];
 
   fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-label/NIXOS_BOOT";
-      fsType = "vfat";
-    };
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
@@ -25,4 +28,34 @@
   };
 
   swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+         hostName = "aarch64.nixos.community";
+         maxJobs = 96;
+         sshKey = "/root/nixos";
+         sshUser = "bkchr";
+         system = "aarch64-linux";
+         supportedFeatures = [ "big-parallel" ];
+      }
+    ];
+  };
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.firmware = [
+    (pkgs.stdenv.mkDerivation {
+     name = "broadcom-rpi3-extra";
+     src = pkgs.fetchurl {
+     url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/54bab3d/brcm80211/brcm/brcmfmac43430-sdio.txt";
+     sha256 = "19bmdd7w0xzybfassn7x4rb30l70vynnw3c80nlapna2k57xwbw7";
+     };
+     phases = [ "installPhase" ];
+     installPhase = ''
+     mkdir -p $out/lib/firmware/brcm
+     cp $src $out/lib/firmware/brcm/brcmfmac43430-sdio.txt
+     '';
+     })
+  ];
 }
