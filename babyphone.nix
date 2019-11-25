1e724fd17edd545b7e1e4e1386d84a51359b90d9
Bastian Köcher <git@kchr.de>
2019-11-25 15:32:42 +0100

    Update

--- babyphone.nix
+++ babyphone.nix
@@ -1,7 +1,39 @@
 { config, pkgs, ... }:
 
 let
-  server = pkgs.callPackage /home/bastian/projects/private/babyphone_app/babyphone-server.nix {};
+  server = pkgs.callPackage /home/bastian/projects/private/babyphone_app/nix/babyphone-server.nix {};
+  kernel = pkgs.linuxPackages_rpi.kernel;
+  seeed-voicecard = pkgs.stdenv.mkDerivation rec {
+    name = "seeed-voicecard";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "respeaker";
+      repo = "seeed-voicecard";
+      rev = "b98fce84e0e45eaa85ef08c4b5dfe962b49a5149";
+      sha256 = "0imsygrs718p305pb3rjgw29cqfx22lkvzbg7myi6cbdxl1nzzi1";
+    };
+
+    #preConfigure = ''
+    #  substituteInPlace Makefile --replace "snd-soc-wm8960-objs := wm8960.o" ""
+    #  substituteInPlace Makefile --replace "obj-m += snd-soc-wm8960.o" ""
+    #'';
+
+    KERNELDIR = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
+
+    NIX_CFLAGS = ["-Wno-error=cpp"];
+
+    nativeBuildInputs = [ pkgs.perl ] ++ kernel.moduleBuildDependencies;
+
+    buildPhase = "make -C $KERNELDIR M=$(pwd) modules";
+    installPhase = ''
+      mkdir -p $out/lib/modules/${kernel.modDirVersion}/sound/soc/codecs
+      mkdir -p $out/lib/modules/${kernel.modDirVersion}/sound/soc/bcm
+
+      cp snd-soc-wm8960.ko $out/lib/modules/${kernel.modDirVersion}/sound/soc/codecs
+      cp snd-soc-ac108.ko $out/lib/modules/${kernel.modDirVersion}/sound/soc/codecs
+      cp snd-soc-seeed-voicecard.ko $out/lib/modules/${kernel.modDirVersion}/sound/soc/bcm
+    '';
+  };
 in
 {
   imports = [
@@ -21,9 +53,9 @@ in
   '';
   services.nixosManual.enable = false;
 
-  boot.kernelParams = [ "cma=32M" ];
+  boot.kernelParams = [ "cma=32M" "console=ttyS1,115200n8" ];
   # Load the camera kernel module by default
-  boot.kernelModules = [ "bcm2835-v4l2" ];
+  boot.kernelModules = [ "bcm2835-v4l2" "snd_bcm2835" ];
 
   fileSystems = {
     "/" = {
@@ -35,29 +67,25 @@ in
   swapDevices = [ { device = "/swapfile"; size = 1024; } ];
 
   hardware.enableRedistributableFirmware = true;
-  hardware.firmware = [
-    (pkgs.stdenv.mkDerivation {
-     name = "broadcom-rpi3-extra";
-     src = pkgs.fetchurl {
-     url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/54bab3d/brcm80211/brcm/brcmfmac43430-sdio.txt";
-     sha256 = "19bmdd7w0xzybfassn7x4rb30l70vynnw3c80nlapna2k57xwbw7";
-     };
-     phases = [ "installPhase" ];
-     installPhase = ''
-     mkdir -p $out/lib/firmware/brcm
-     cp $src $out/lib/firmware/brcm/brcmfmac43430-sdio.txt
-     '';
-     })
-  ];
 
-  # Enable pulseaudio
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.package = pkgs.pulseaudioFull;
+  # Enable sound
+  sound.enable = true;
+  sound.extraConfig = ''
+    pcm.!default {
+        type hw
+        card 1
+    }
 
-  boot.kernelPackages = pkgs.linuxPackages_testing;
+    ctl.!default {
+        type hw
+        card 1
+    }
+  '';
 
   # Enable the networkmanager
   networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
+  networking.enableIPv6 = false;
   #environment.noXlibs = true;
 
   # Open the babyphone server port for local reachability
@@ -86,18 +114,29 @@ in
       '';
 
       environment = {
-        RUST_LOG = "info";
+        RUST_LOG = "info,picoquic=trace";
         RUST_BACKTRACE = "1";
       };
 
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${server}/bin/babyphone-server --client_ca_path /var/lib/babyphone/client_ca_path --server_ca_path /var/lib/babyphone/server_ca_path";
+        ExecStart = "${server}/bin/babyphone-server --client_ca_path /var/lib/babyphone/client_ca_path --server_ca_path /var/lib/babyphone/server_ca_path --settings /var/lib/babyphone/settings.json";
         Restart = "always";
+	RestartSec = "1s";
         User = "babyphone";
       };
 
-      after = [ "network.target" ];
+      unitConfig = {
+        StartLimitIntervalSec = "0";
+      };
+
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
       wantedBy = [ "default.target" ];
+      #onFailure = [ "systemd-reboot.service" ];
     };
+
+  fonts.fontconfig.penultimate.enable = false;
+  #boot.kernelPackages = pkgs.linuxPackages_rpi3;
+  #boot.extraModulePackages = [ seeed-voicecard ];
 }
