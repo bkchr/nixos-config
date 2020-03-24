cdf2401c32c87477b1ce3dd0d4672d931f8f6241
Bastian Köcher <git@kchr.de>
2020-03-24 22:12:29 +0100

    Enable steam

--- work-dell-precision.nix
+++ work-dell-precision.nix
@@ -34,5 +34,11 @@
 
   environment.systemPackages = with pkgs; [
     zoom-us
+    steam
   ];
+
+  # Enable 32bit support for Steam
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.pulseaudio.support32Bit = true;
 }
