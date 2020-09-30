e5f304fef1d6d12a0d04a92c07af67aa55624d54
Bastian Köcher <git@kchr.de>
2020-09-30 22:51:15 +0200

    Add nur

--- system-with-gui-configuration.nix
+++ system-with-gui-configuration.nix
@@ -130,6 +130,12 @@ in
   nixpkgs.config = {
     firefox.enablePlasmaBrowserIntegration = true;
     android_sdk.accept_license = true;
+
+    packageOverrides = pkgs: {
+      nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+        inherit pkgs;
+      };
+    };
   };
 
   services.syncthing.enable = true;
