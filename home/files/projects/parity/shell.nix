f57202a2a6f085f4d47c3578091ab40f5747252d
Bastian Köcher <git@kchr.de>
2020-10-07 09:54:48 +0200

    Update rust

--- home/files/projects/parity/shell.nix
+++ home/files/projects/parity/shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 let
   pkgs = import <nixpkgs> {};
-  myrust = ((rustChannelOf { date = "2020-09-17"; channel = "nightly"; }).rust.override { extensions = [ "rust-src" "rust-analysis" "rustfmt-preview" ]; targets = [ "wasm32-unknown-unknown" ]; });
+  myrust = ((rustChannelOf { date = "2020-10-04"; channel = "nightly"; }).rust.override { extensions = [ "rust-src" "rust-analysis" "rustfmt-preview" ]; targets = [ "wasm32-unknown-unknown" ]; });
 in
   pkgs.mkShell {
     buildInputs = [
